3d4422b44f59411706c2c6fb73ad57cc64ae3311
Alejandro Rodriguez Romero <alex98rodriguezromero@gmail.com>
2023-12-25 08:22:50 -0500

    feature: add ng zorro feature: add font (google link) refactor: update login view (no logic)

--- src/app/modules/auth/views/login/login.view.ts
+++ src/app/modules/auth/views/login/login.view.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { NzButtonComponent } from "ng-zorro-antd/button";
+import { NzInputDirective, NzInputGroupComponent } from "ng-zorro-antd/input";
+import { NzIconDirective } from "ng-zorro-antd/icon";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    NzButtonComponent,
+    NzInputDirective,
+    NzIconDirective,
+    NzInputGroupComponent
+  ],
   templateUrl: './login.view.html',
   styleUrl: './login.view.scss'
 })
