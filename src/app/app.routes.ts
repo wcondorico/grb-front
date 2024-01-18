296d7f7ce1c6f7ca8df8b099ad8bf70ecde99531
Alejandro Rodriguez <alex98rodriguezromero@gmail.com>
2024-01-17 22:30:55 -0300

    refactor: add name to authentication feat: add directories for benefits

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'dashboard',
     loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
   }
 ];
