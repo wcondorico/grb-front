df18119df5184e4053b565b595965e9fd46a7d54
William <williamcondori50@gmail.com>
2024-01-23 12:02:22 -0500

    refactor: new format of imports

--- src/app/modules/benefits/views/home/home.view.ts
+++ src/app/modules/benefits/views/home/home.view.ts
@@ -3,6 +3,7 @@ import { NzButtonComponent } from 'ng-zorro-antd/button';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { RouterLink } from "@angular/router";
 
+
 @Component({
   selector: 'app-home',
   standalone: true,
