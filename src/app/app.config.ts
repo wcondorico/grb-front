4dab2b937e2e6fcb158a2a843d6d59d3b4f95839
William <williamcondori50@gmail.com>
2024-02-12 00:00:18 -0500

    Refactor: responsive refactorized

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,13 @@
 import { ApplicationConfig, importProvidersFrom, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
-import { es_ES, provideNzI18n } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import localeEsPE from '@angular/common/locales/es-PE';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { provideAnimations } from '@angular/platform-browser/animations';
+
+import { routes } from './app.routes';
+import { es_ES, provideNzI18n } from 'ng-zorro-antd/i18n';
 
 registerLocaleData(localeEsPE, 'es-PE');
 
