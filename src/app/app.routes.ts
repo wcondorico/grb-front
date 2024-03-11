8ef4ef0e5af02566fcf66bdf6602700170de9321
Alejandro Rodriguez <alex98rodriguezromero@gmail.com>
2024-03-11 18:07:51 -0500

    refactor: remove route benefits

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,6 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '',
-    loadChildren: () => import('./modules/benefits/benefits.module').then(m => m.BenefitsModule)
-  },
-  {
     path: 'auth',
     loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
   }
