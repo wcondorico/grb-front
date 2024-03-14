c89e8db080be9aaae7152e49fb963069f5b9b5c4
Gerson Zavaleta <gzavaletae@intercorp.com.pe>
2024-03-13 21:29:24 -0500

    feat: add endpoint

--- src/app/features/auth/infrastructure/http/sign-in.http.ts
+++ src/app/features/auth/infrastructure/http/sign-in.http.ts
@@ -2,6 +2,7 @@ import { SignInRepository } from "../../domain/repository/sign-in.repository";
 import { Observable } from "rxjs";
 import { inject, Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { environment } from "../../../../../environments/environment";
 
 @Injectable()
 export class SignInHttp extends SignInRepository {
@@ -10,7 +11,7 @@ export class SignInHttp extends SignInRepository {
 
   signInWithPassword(email: string, password: string): Observable<any> {
     return this.http.post(
-      ``,
+      `${environment.api}/auth/sign-in`,
       {
         email: email,
         password: password
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-
+  api: 'https://temporaldomainapis.com'
 };
